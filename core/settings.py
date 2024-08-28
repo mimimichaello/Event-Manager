e23e85edbdf076fb1f1c43c1b0786a42b24d9661
mimimichaello <137781824+mimimichaello@users.noreply.github.com>
2024-08-28 15:26:23 +0300

    Added settings for sending mail

--- core/settings.py
+++ core/settings.py
@@ -135,3 +135,12 @@ AUTHENTICATION_BACKENDS = [
 
 
 CELERY_BROKER_URL = "redis://redis:6379/0"
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
